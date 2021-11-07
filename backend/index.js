fe7af1be0779ca3ea9df55dabf0ba27ec62424c7
Adam <adam@linebooker.com>
2021-11-07 21:25:02 +0200

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log("ajb req", req.query);
+  res.send("Hello World!" + req.query);
+});
+
+app.post("/create-product-expiry-reminder", (req, res) => {
+  // First read existing users.
+  // fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+  //    data = JSON.parse( data );
+  //    data["user4"] = user["user4"];
+  //    console.log( data );
+  //    res.end( JSON.stringify(data));
+  // });
+
+  console.log("ajb body", JSON.stringify(req.body));
+
+  const expiryReminderData = JSON.stringify(req.body);
+
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
