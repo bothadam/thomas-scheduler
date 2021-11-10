17cdc5320258f1f68c92870f6878e8216f544dfe
Adam <adam@linebooker.com>
2021-11-10 09:08:55 +0200

    add material ui - do json post request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Scheduler from "./components/Scheduler";
+import DateAdapter from "@mui/lab/AdapterMoment";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
 
 function App() {
   return (
-    <div className="App">
-      <Scheduler />
-    </div>
+    <LocalizationProvider dateAdapter={DateAdapter}>
+      <div className="App">
+        <Scheduler />
+      </div>
+    </LocalizationProvider>
   );
 }
 
--- frontend/src/components/Scheduler.js
+++ frontend/src/components/Scheduler.js
@@ -1,20 +1,50 @@
-import React from "react";
-import Button from "@mui/material/Button";
+import React, { useState } from "react";
 import "./Scheduler.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import DatePicker from "@mui/lab/DatePicker";
 
 function Scheduler({}) {
+  const [itemName, setItemName] = useState("");
+  const [expiryDate, setExpiryDate] = useState(null);
   const { REACT_APP_API_URL } = process.env;
 
   return (
     <div className="">
-      <form
-        action={`${REACT_APP_API_URL}/create-product-expiry-reminder`}
-        method="post"
+      <TextField
+        value={itemName}
+        label="Item Name"
+        onChange={(e) => setItemName(e.target.value)}
+      />
+      <DatePicker
+        label="Expiry Date"
+        value={expiryDate}
+        onChange={(newValue) => {
+          setExpiryDate(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+      <Button
+        onClick={() => {
+          var xhr = new XMLHttpRequest();
+          xhr.open(
+            "POST",
+            `${REACT_APP_API_URL}/create-product-expiry-reminder`,
+            true
+          );
+          xhr.setRequestHeader("Content-Type", "application/json");
+
+          xhr.send(
+            JSON.stringify({
+              "item-name": itemName,
+            })
+          );
+        }}
+        id="hey"
+        variant="contained"
       >
-        <input type="text" name="item-name" />
-        <input type="date" name="expiry-date" />
-        <input type="submit" />
-      </form>
+        hey
+      </Button>
     </div>
   );
 }
