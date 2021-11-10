c0d371eb96a75fce081a08798ca5c8f43941a026
Adam <adam@linebooker.com>
2021-11-10 09:09:12 +0200

    fix cors issues

--- backend/backend.js
+++ backend/backend.js
@@ -1,9 +1,29 @@
 const express = require("express");
+var cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const port = 3000;
+
+const allowedOrigins = ["http://localhost:3000", "http://34.105.157.177/"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   console.log("ajb req", req.query);
@@ -11,8 +31,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create-product-expiry-reminder", (req, res) => {
-  console.log("ajb body", JSON.stringify(req.body));
-
   const expiryReminderData = JSON.stringify(req.body);
 
   res.sendStatus(200);
