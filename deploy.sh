24d20840a52d0419ccef802abc1921c9d8b7cd15
Adam <adam@linebooker.com>
2021-11-11 09:29:10 +0200

    deploy script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cd ~/thomas-scheduler
+cd /home/adambotha007/thomas-scheduler
 
 echo "Pulling latest changes"
 sudo git checkout master
@@ -7,10 +7,10 @@ sudo git pull
 
 echo "Restarting backend"
 sudo pm2 stop backend
-cd ~/thomas-scheduler/backend
+cd /home/adambotha007/thomas-scheduler/backend
 sudo pm2 start backend
 
 echo "Rebuild frontend and restart nginx"
-cd ~/thomas-scheduler/frontend
+cd /home/adambotha007/thomas-scheduler/frontend
 sudo npm run build
 sudo systemctl restart nginx
